813f068fb12c8afff72e58974659c9a12b826301
Wesley Havens <wesleydh85@gmail.com>
2021-06-09 12:24:57 -0500

    Restart webpage.

--- scriptJS.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    let footer = document.getElementById("footer")
-    //footer.append("Last updated: February 14th, 2020");
-    footer.append(`Website last updated February 14th, 2021`);
-    //document.getElementById("demo").innerHTML = d.getFullYear();
-});
-
