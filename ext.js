7cd8a6f30fbdf316dd9745ef88459adaecb62cf6
Wesley Havens <wesleydh85@gmail.com>
2021-07-17 22:56:37 -0500

    Responsiveness added.

--- ext.js
+++ ext.js
@@ -11,3 +11,14 @@ window.addEventListener("scroll", () => {
     //     scroller.innerHTML = '<h6>Scroll down for more</h6>';
     // }
 });
+
+
+function toggleMenu() {
+    let menuToggle = document.querySelector(".toggle");
+    let menu = document.querySelector(".menu");
+
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("active");
+
+
+}
